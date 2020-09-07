c40cf0d7da3b745bf85df5aec4ee5c89d08e0024
IMHOJEONG <39ghwjd@naver.com>
2020-09-08 01:28:20 +0900

    Add cors package

--- servers/rest/rest.go
+++ servers/rest/rest.go
@@ -4,7 +4,22 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/gin-contrib/static"
 	cors "github.com/rs/cors/wrapper/gin"
+	"encoding/json"
+	"io/ioutil"
+	"fmt"
+	"net/http"
 )
+
+
+type Info struct {
+	Title       string `json:"title"`
+	Date        string `json:"date"`
+	Explanation string `json:"explanation"`
+	Url         string `json:"url"`
+	HdUrl       string `json:"hdurl"`
+	//MediaType      string `json:"media_type"`
+	//ServiceVersion string `json:"service_version"`
+}
 
 func RunAPI(address string) error {
 	// Gin 엔진
@@ -21,9 +36,43 @@ func RunAPI(address string) error {
 	// r.GET("/search", IndexHome)
 	// run server
 	r.Use(cors.Default())
-	r.GET("/search", IndexHome)
+	// r.GET("/search", IndexHome)
+
+	r.GET("/search", func(c *gin.Context){
+	
+		// get values from API
+		// 	"github.com/GiterLab/urllib"
+		// req := urllib.Get("https://api.nasa.gov/planetary/apod?api_key=rvU2JWqSHNFizqfke1599aJG4Ax3GvKmQYXPfSld&hd=true")
+		
+		resp, err := http.Get("https://api.nasa.gov/planetary/apod?api_key=rvU2JWqSHNFizqfke1599aJG4Ax3GvKmQYXPfSld&hd=true")
+	
+		// req.Debug(true)
+		// strJson, err := req.String()
+		if err != nil {
+			fmt.Println(err)
+		}
+		// req.Heade;
+		var info Info
+		defer resp.Body.Close()
+		
+		// // JSON decoding
+		// err = json.Unmarshal([]byte(strJson), &info)
+		// if err != nil {
+		// 	fmt.Println("Parsing error!!")
+		// }
+	
+		// c.JSON(http.StatusOK, info)
+		strJson, err := ioutil.ReadAll(resp.Body)
+		err = json.Unmarshal([]byte(strJson), &info)
+		if err != nil {
+			fmt.Println("Parsing error!!")
+		}
+	
+		c.JSON(http.StatusOK, info)
+	})
+	
+
 	r.Use(static.Serve("/", static.LocalFile("../client/build", true)))	// r.StaticFile("/show","./build/index.html")
-	// r.Static("/","../../build");
 	return r.Run(address)
 }
 
