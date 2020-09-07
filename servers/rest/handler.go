3d7e8778e2983ad248292c05843827a06941b45b
IMHOJEONG <39ghwjd@naver.com>
2020-09-07 16:11:37 +0900

    Add cors package

--- servers/rest/handler.go
+++ servers/rest/handler.go
@@ -29,7 +29,7 @@ func IndexHome(c *gin.Context) {
 	
 	// get values from API
 	req := urllib.Get("https://api.nasa.gov/planetary/apod?api_key=rvU2JWqSHNFizqfke1599aJG4Ax3GvKmQYXPfSld&hd=true")
-	.Header("Access-Control-Allow-Origin",'*')
+	
 
 	req.Debug(true)
 	strJson, err := req.String()
