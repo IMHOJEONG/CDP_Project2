19aa273e9df65e8f1cc1b3be1503c40401baba67
IMHOJEONG <39ghwjd@naver.com>
2020-09-07 21:09:07 +0900

    Add cors package

--- servers/go.mod
+++ servers/go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	github.com/GiterLab/urllib v0.0.0-20200820124023-82571a63c776
+	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-contrib/static v0.0.0-20200815103939-31fb0c56a3d1
 	github.com/gin-gonic/contrib v0.0.0-20200810162008-6dee08bf958e
 	github.com/gin-gonic/gin v1.6.3
--- servers/rest/rest.go
+++ servers/rest/rest.go
@@ -3,11 +3,20 @@ package rest
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/gin-contrib/static"
+	"github.com/gin-contrib/cors"
 )
+
 
 func RunAPI(address string) error {
 	// Gin 엔진
 	r := gin.Default()
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
 	// r.GET("/search", IndexHome)
 	// run server
 	r.GET("/search", IndexHome)
